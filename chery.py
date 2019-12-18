b01556f826855bd932b6646cbc1e6f938562bcc9
HexPang <hexpang@live.com>
2019-12-18 14:19:20 +0800

    Update chery.py

--- chery.py
+++ chery.py
@@ -8,11 +8,12 @@ import uuid
 from urllib import parse
 import os.path
 
-class chery():
+class chery_api():
     # 奇瑞 智云互联 API
-    def __init__(self):
+    def __init__(self, device_id = None):
         self.base_url = "https://cloudriveapp.mychery.com:3082/"
         self.user = None
+        self.device_id = device_id
 
     def gen_uuid(self):
         uuid_str = str(uuid.uuid4())
@@ -63,18 +64,22 @@ class chery():
         return self.request_command(request_data)
 
 
+    def encode_pwd(self, password):
+        sha256_pwd = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        base64_pwd = base64.b64encode(sha256_pwd.encode('utf-8')).decode('utf-8')
+        return base64_pwd
+
+
     def login(self, username, password, token):
         cache = self.read_cache("user")
         if cache is not None:
             self.user = cache
             return self.user
-        sha256_pwd = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        base64_pwd = base64.b64encode(sha256_pwd.encode('utf-8')).decode('utf-8')
         data = {
             "data": {
                 "appType": "1",
                 "userAccount": username,
-                "userPasswd":  base64_pwd,
+                "userPasswd":  self.encode_pwd(password),
                 "token": token
             }
         }
@@ -83,18 +88,24 @@ class chery():
         return self.user
 
 
-    def load_vehicle_list(self):
+    def refresh_global_id(self, union_id, app_type = "1"):
+        res = self.request(100, 102, {"data": {"token": self.user["data"]["token"], "unionid": union_id, "appType": app_type}})
+        self.user = res
+        return self.user
+
+
+    def vehicle_list(self):
         return self.request(100, 900)
 
 
-    def load_vehicle_config(self, vin):
+    def vehicle_config(self, vin):
         return self.request(100, 969, {"data": {vin: vin}})
 
 
-    def load_vehicle(self):
+    def vehicle_info(self, vin):
         data = {
             "data": {
-                "vin": self.user["data"]["defaultVin"]
+                "vin": vin
             }
         }
         res = self.request(100, 910, data)
@@ -103,3 +114,21 @@ class chery():
 
     def load_user(self):
         return self.request(100, 112)
+
+
+    def upload_device(self):
+        return self.request(100, 1004, {"data": {"deviceId": self.device_id }})
+
+    
+    def menu(self, vin):
+        return self.request(100, 969, {"data": {"vin": vin, "isBluetoothAuth": 0}})
+
+
+    def vehicle_pass(self, vin, password):
+        return self.request(100, 125, {"data": {"vin": vin, "openGesturePass": "0", "vehiclePass": self.encode_pwd(password)}})
+
+
+    def vehicle_command(self, vin, password, data, passwordType = "0", instruction = "1105"):
+        conf_data = {"vin": vin, "safePasswd": self.encode_pwd(password), "passwordType": "0", "instruction": instruction}
+        merge_data = {**data, **conf_data}
+        return self.request(100, 970, {"data": merge_data})
